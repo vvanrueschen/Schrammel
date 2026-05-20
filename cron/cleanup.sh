08dacafda0655e96d3976ba3d0ece986f17de663
vvanrueschen <vvanrueschen@gmail.com>
2026-05-20 22:01:04 +0200

    feat: make ports and external URLs configurable via .env

--- cron/cleanup.sh
+++ cron/cleanup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sf -X POST -H "X-Cleanup-Token: $CLEANUP_SECRET_TOKEN" http://schrammel:3000/api/cleanup-worst-songs
+curl -sf -X POST -H "X-Cleanup-Token: $CLEANUP_SECRET_TOKEN" http://schrammel:${APP_PORT:-3000}/api/cleanup-worst-songs
